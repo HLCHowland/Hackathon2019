aa8de49835cf2a9b5c19df8ccda64cb9a7bdbda1
NullDefault <davisha999@gmail.com>
2019-11-09 17:56:44 -0500

    fixed data

--- src/main/java/com/example/MockData.java
+++ src/main/java/com/example/MockData.java
@@ -3,10 +3,14 @@ package com.example;
 import java.util.HashMap;
 
 public class MockData {
-    public HashMap<String, String> data = new HashMap<String,String>();
-    data.put("What color hair does bobby have", "Black");
-    data.put("How old is David", "Nineteen");
-    data.put("Who's super cool","Henry");
-    data.put("Hail who?","Hail google");
-    data.put("What do we say to game freak?","Fuck you");
+    public HashMap<String,String> data = new HashMap<String, String>();
+    public MockData(){
+        data = new HashMap<String,String>();
+        data.put("What color hair does bobby have", "Black");
+        data.put("How old is David", "Nineteen");
+        data.put("Who's super cool","Henry");
+        data.put("Hail who?","Hail google");
+        data.put("What do we say to game freak?","Fuck you");
+    }
+
 }

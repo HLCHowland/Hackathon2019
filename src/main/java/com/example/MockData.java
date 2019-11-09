b0ca783a125a4333ae35d06f8f0cfcfd0cc5ab4e
Robert Ludwick <rludwick@drew.edu>
2019-11-09 17:58:14 -0500

    merge conflict fix

--- src/main/java/com/example/MockData.java
+++ src/main/java/com/example/MockData.java
@@ -1,12 +1,15 @@
 package com.example;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class MockData {
-    public HashMap<String, String> data = new HashMap<String,String>();
-    data.put("What color hair does bobby have", "Black");
-    data.put("How old is David", "Nineteen");
-    data.put("Who's super cool","Henry");
-    data.put("Hail who?","Hail google");
-    data.put("What do we say to game freak?","Fuck you");
+    public static void main(String[] args) {
+        public HashMap<String, String> data = new HashMap<String, String>();
+        data.put("What color hair does bobby have", "Black");
+        data.put("How old is David", "Nineteen");
+        data.put("Who's super cool", "Henry");
+        data.put("Hail who?", "Hail google");
+        data.put("What do we say to game freak?", "Fuck you");
+    }
 }
